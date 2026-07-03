7d1b9c0df6245497b3102988ac2763c3941ef71f
Chintan616 <chintukasundra060106@gmail.com>
2026-07-03 22:22:03 +0530

    feat : return 0 for empty string

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,7 @@
 public class StringCalculator {
-    
+    public int add(String numbers){
+
+        if(numbers == null || numbers.isEmpty()) return 0;
+        return -1;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,6 +3,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
 
-    
+    @Test
+    public void empty_string_should_return_zero(){
+
+        //instance
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(0,calculator.add(""));
+    }
 }
 
